fc87655c9fac5dd47531969151bd3f16f0a5a472
thiago jose de almeida simao <thiago.jsimao.me@gmail.com>
2018-10-19 23:43:05 -0300

    alter structure model

---
+++ Init.js
@@ -0,0 +1,3 @@
+const Initialize = require("./Crawler/start.js");
+
+Initialize();
--- config/conf.js
+++ config/conf.js
@@ -1,6 +1,6 @@
 const config = {
   url:
-    "https://www.google.com.tw/search?q=sal%C3%A3o+de+beleza&tbs=lrf:!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:2&tbm=lcl&ei=u1jKW9RmhZXABLbWq0g&start=40&sa=N#rlfi=hd:;si:;mv:!3m12!1m3!1d4325.865988237908!2d-46.8501634!3d-23.50074665!2m3!1f0!2f0!3f0!3m2!1i186!2i227!4f13.1;start:0"
+    "https://www.google.com.br/search?hl=pt-BR&tbm=lcl&ei=wZTKW7vZDI6uwgTFlJfAAQ&q=sal%C3%A3o+de+beleza+osasco&oq=sal%C3%A3o+de+beleza+osasco&gs_l=psy-ab.3...0.0.0.144623.0.0.0.0.0.0.0.0..0.0....0...1c..64.psy-ab..0.0.0....0.KoOWp7H52OU#rlfi=hd:;si:;mv:!3m12!1m3!1d24809.070256064035!2d-46.785440799999996!3d-23.515213799999998!2m3!1f0!2f0!3f0!3m2!1i376!2i651!4f13.1;tbs:lrf:!2m1!1e2!2m1!1e3!2m4!1e17!4m2!17m1!1e2!3sIAE,lf:1,lf_ui:2"
 };
 
 module.exports = config;
